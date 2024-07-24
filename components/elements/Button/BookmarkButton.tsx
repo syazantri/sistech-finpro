0aa43a598c62852651afe840856f663998e07ce6
Syazantri Salsabila <syazantri@gmail.com>
2024-07-24 14:13:42 +0700

    fix

--- components/elements/Button/BookmarkButton.tsx
+++ components/elements/Button/BookmarkButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface BookmarkButtonProps {
   isBookmark: boolean;
@@ -7,7 +8,8 @@ interface BookmarkButtonProps {
 const BookmarkButton: React.FC<BookmarkButtonProps> = ({ isBookmark }) => {
   return (
     <button className={`bookmark-button ${isBookmark ? 'bookmarked' : ''}`}>
-      {isBookmark ? 'Bookmarked' : 'Bookmark'}
+      
+      {isBookmark ? <Image src="/images/IsBookmark=True.png" alt="bookmarked" width="33" height="33" /> : <Image src="/images/IsBookmark=False.png" alt="bookmark" width="33" height="33" /> }
     </button>
   );
 };
